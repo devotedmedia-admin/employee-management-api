e7d026acc36d8dd7ac9c030f3856f07bae4c8eff
devotedmedia-admin <109428919+devotedmedia-admin@users.noreply.github.com>
2023-03-30 10:02:51 +0200

    initial commit

---
+++ Employees/Employees.API/Controllers/EmployeesController.cs
@@ -0,0 +1,82 @@
+﻿using Employees.API.Data;
+using Employees.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Employees.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly EmployeeDbContext _employeeDbContext;
+
+        public EmployeesController(EmployeeDbContext employeeDbContext)
+        {
+            _employeeDbContext = employeeDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllEmployees()
+        {
+            var employees = await _employeeDbContext.Employees.ToListAsync();
+            return Ok(employees);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddEmployee([FromBody] Employee employeeRequest)
+        {
+            employeeRequest.Id = Guid.NewGuid();
+            await _employeeDbContext.AddAsync(employeeRequest);
+            await _employeeDbContext.SaveChangesAsync();
+
+            return Ok(employeeRequest);
+        }
+
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> GetEmployee([FromRoute] Guid id)
+        {
+            var employee = await _employeeDbContext.Employees.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (employee == null)
+                return NotFound();
+            return Ok(employee);
+        }
+
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> UpdateEmployee([FromRoute] Guid id, Employee updateEmployeeRequest)
+        {
+            var employee = await _employeeDbContext.Employees.FindAsync(id);
+
+            if(employee == null)
+                return NotFound();
+
+            employee.Name = updateEmployeeRequest.Name;
+            employee.Email = updateEmployeeRequest.Email;
+            employee.Phone = updateEmployeeRequest.Phone;
+            employee.Salary = updateEmployeeRequest.Salary;
+            employee.Department = updateEmployeeRequest.Department;
+            await _employeeDbContext.SaveChangesAsync();
+
+            return Ok(employee);
+        }
+
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> DeleteEmployee([FromRoute] Guid id)
+        {
+            var employee = await _employeeDbContext.Employees.FindAsync(id);
+
+            if (employee == null)
+                return NotFound();
+
+            _employeeDbContext.Employees.Remove(employee);
+            await _employeeDbContext.SaveChangesAsync();
+
+            return Ok(employee);
+        }
+    }
+}
---
+++ Employees/Employees.API/Data/EmployeeDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Employees.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Employees.API.Data
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
